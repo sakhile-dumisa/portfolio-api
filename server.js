939a069e0dea7fb641c5409f776ce6d2baeed855
dumisa-sakhile <contact.dumisa@gmail.com>
2025-11-19 10:45:44 +0200

    Fix static file serving to correctly serve favicon.ico from public directory

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public/favicon.ico'));
 
 
 // Initialize Resend
